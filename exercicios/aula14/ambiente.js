b86f6a206e20867ffcc8da1b2edcf7aaeba4bc32
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2022-04-29 14:14:05 -0300

    Aula 14

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,22 @@
+/*
+var c = 1
+
+while (c <= 5) {
+    console.log(`Vez ${c}`)
+    c++
+}
+*/
+
+//--------------------------------------------
+
+/*
+for (var c = 1;c <= 10000;c++) {
+    console.log(`Vez ${c}`)
+}
+*/
+
+console.log('Vai começar...')
+for (var c = 1;c <=5;c++) {
+    console.log(`Vez ${c}`)
+}
+console.log('FIM!')
