dfcffdf53b01d4bf123ff5e776b098bbed4d0b97
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2022-04-29 13:56:00 -0300

    Aula 13 do curso de JavaScript do Curso em Vídeo

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+/*
+var contador = 1
+
+while( contador <= 6) {
+console.log(`Vez ${contador}`)
+contador ++
+}
+*/
