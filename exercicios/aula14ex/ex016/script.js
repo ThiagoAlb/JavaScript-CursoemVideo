bf29829968fc44fc8e2e57931a9491088180dad1
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2022-05-02 15:19:11 -0300

    ex016 e ex017
    Finalização do exercício 016
    Início e finalização do exercício 017

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,10 +1,34 @@
 function contar () {
-    var inicio = window.document.getElementById('inicio')
-    var fim = window.document.getElementById('fim')
-    var passa = window.document.getElementById('passo')
+    var inicio = window.document.getElementById('txtinicio')
+    var fim = window.document.getElementById('txtfim')
+    var passa = window.document.getElementById('txtpasso')
     var res = window.document.getElementById('res')
 
-    for (var inicio = inicio; inicio <= fim; passa++) {
-        res.innerHTML = `${passa}`
+    if (inicio.value.length == 0 || fim.value.length == 0 || passa.value.lenght == 0) {
+        res.innerHTML = `Não é possível contar!`
+        window.alert(`[ERRO] Faltam dados!`)
+        
+    } else {
+        res.innerHTML = `Contando: <br>`
     }
+
+    var i = Number(inicio.value)
+    var f = Number(fim.value)
+    var p = Number(passa.value)
+    if (p <= 0) {
+        window.alert('Passo inválido! Considerando Passo = 1')
+        p = 1
+    }
+    if (i < f) {
+        //Vai contar de maneira crescente
+        for (var c = i; c <= f ; c = c + p) {
+            res.innerHTML += ` ${c} \u{27A1}`
+        }
+    } else {
+        //Vai contar de forma decrescente
+        for(let c = i; c >= f; c -= p)
+        res.innerHTML += ` ${c} \u{1F449} ` 
+    }
+    res.innerHTML += `\u{1F3C1}`
+    
 }
--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function tabuada() {
+    let num = window.document.getElementById('txtnumero')
+    let tab = window.document.getElementById('seltabuada')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+    
+}
