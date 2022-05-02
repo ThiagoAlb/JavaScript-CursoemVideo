3606fe3c52fa0801439063e414705bbba4048888
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2022-05-02 09:27:40 -0300

    Exercicio 17

---
+++ exercicios/aula14ex/ex016/script.js
@@ -0,0 +1,10 @@
+function contar () {
+    var inicio = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var passa = window.document.getElementById('passo')
+    var res = window.document.getElementById('res')
+
+    for (var inicio = inicio; inicio <= fim; passa++) {
+        res.innerHTML = ``
+    }
+}
